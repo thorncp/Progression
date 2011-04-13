c381eefd9d71f321265364566f9888519d1b837e
thorncp <thorncp@gmail.com>
2011-04-12 21:24:20 -0800

    more features!

--- Progression/ProgressBar.cs
+++ Progression/ProgressBar.cs
@@ -4,7 +4,7 @@ namespace Progression
 {
     public class ProgressBar
     {
-        public ProgressBar (int total, string title = null)
+        public ProgressBar(int total,string title = null)
         {
             Total = total;
             Title = title;
@@ -15,6 +15,16 @@ namespace Progression
         public int Status { get; set; }
 
         public string Title { get; set; }
+
+        public void Bump()
+        {
+            UpdateStatus(Status + 1);
+        }
+
+        public void UpdateStatus(int status)
+        {
+            Status = Math.Max(Math.Min(status, Total), 0);
+        }
     }
 }
 
--- TestProgression/TestProgressBar.cs
+++ TestProgression/TestProgressBar.cs
@@ -10,29 +10,70 @@ namespace TestProgression
         [Test]
         public void TestNewProgressBarHasCorrectTotal()
         {
-            var progressBar = new ProgressBar (100);
-            Assert.AreEqual (100, progressBar.Total);
+            var progressBar = new ProgressBar(100);
+            Assert.AreEqual(100, progressBar.Total);
         }
 
         [Test]
         public void TestNewProgressBarHasZeroStatus()
         {
-            var progressBar = new ProgressBar (100);
-            Assert.AreEqual (0, progressBar.Status);
+            var progressBar = new ProgressBar(100);
+            Assert.AreEqual(0, progressBar.Status);
         }
 
         [Test]
         public void TestNewProgressBarWithTitleShouldRetainTitle()
         {
-            var progressBar = new ProgressBar (100, "Yo Dawg");
-            Assert.AreEqual ("Yo Dawg", progressBar.Title);
+            var progressBar = new ProgressBar(100, "Yo Dawg");
+            Assert.AreEqual("Yo Dawg", progressBar.Title);
         }
 
         [Test]
         public void TestNewProgressBarWithoutTitleShouldHaveNullTitle()
         {
-            var progressBar = new ProgressBar (100);
+            var progressBar = new ProgressBar(100);
             Assert.IsNull(progressBar.Title);
+        }
+
+        [Test]
+        public void TestBumpingTheProgressBarShouldIncreaseTheStatusByOne()
+        {
+            var progressBar = new ProgressBar(100);
+            progressBar.Bump();
+            Assert.AreEqual(1, progressBar.Status);
+        }
+
+        [Test]
+        public void TestSetStatusShouldUpdateTheCurrentStatus()
+        {
+            var progressBar = new ProgressBar(100);
+            progressBar.UpdateStatus(50);
+            Assert.AreEqual(50, progressBar.Status);
+        }
+
+        [Test]
+        public void TestUpdatingStatusHigherThanTotalForcesStatusToTotal()
+        {
+            var progressBar = new ProgressBar(100);
+            progressBar.UpdateStatus(150);
+            Assert.AreEqual(100, progressBar.Status);
+        }
+
+        [Test]
+        public void TestUpdatingStatusBelowZeroForcesStatusToZero()
+        {
+            var progressBar = new ProgressBar(100);
+            progressBar.UpdateStatus(-50);
+            Assert.AreEqual(0, progressBar.Status);
+        }
+
+        [Test]
+        public void TestBumpDoesNotUpdateStatusHigherThanTotal()
+        {
+            var progressBar = new ProgressBar(100);
+            progressBar.UpdateStatus(100);
+            progressBar.Bump();
+            Assert.AreEqual(100, progressBar.Status);
         }
     }
 }
