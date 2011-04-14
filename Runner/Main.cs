0d6a20841c520586d042fca0d94686d81508660d
thorncp <thorncp@gmail.com>
2011-04-13 21:13:29 -0800

    add runner project to see the progress bar in action

---
+++ Runner/Main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading;
+using Progression;
+
+namespace Runner
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var progressBar = new ProgressBar(100, "Herp Derp");
+            for (int i = 0; i < 100; i++) {
+                Thread.Sleep(100);
+                progressBar.Bump();
+            }
+        }
+    }
+}
