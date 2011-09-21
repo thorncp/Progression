147d40b7cfa4e59479e50f9115990196df6d254f
Chris Thorn <thorncp@gmail.com>
2011-09-20 18:00:16 -0800

    Update Runner main class with examples.

--- Runner/Main.cs
+++ Runner/Main.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Progression;
 
 namespace Runner
@@ -7,11 +10,26 @@ namespace Runner
     {
         public static void Main(string[] args)
         {
-            var progressBar = new ProgressBar(50000, "Herp Derp", 30);
-            for (int i = 0; i < 50000; i++) {
+            var progressBar = new ProgressBar(5000, "Standard", 30);
+
+            for (int i = 0; i < 5000; i++) {
                 Thread.Sleep(1);
                 progressBar.Bump();
             }
+
+            ProgressBar.ForEach(Enumerable.Range(0, 5000), "Printing!", 30, i => {
+                // print every 100th int, directly to the console
+                Thread.Sleep(1);
+                if (i % 100 == 0) Console.Out.WriteLine(i);
+            });
+
+            // the progress bar is not thread safe, we must take extract steps when multi threading
+            var threadedProgressBar = new ProgressBar(5000, "Threading, ZOMG!", 30);
+            Parallel.ForEach(Enumerable.Range(0, 5000), i => {
+                Thread.Sleep(1);
+                // notice the lock!
+                lock (threadedProgressBar) threadedProgressBar.Bump();
+            });
         }
     }
 }
